63554d5832f529cba27c8ef454e0a8fac772045c
Jerome Moore <Romeski@Ks-Mac-mini.home>
2016-05-16 17:42:58 -0400

    live v2

--- js/tweaktween.js
+++ js/tweaktween.js
@@ -1,7 +1,7 @@
 $(function(){
 				
 				var $startAnim = $("div.navbar-header")
-				var $demoText = $("a#button,navbar-brand").css("font-size", "50px");
+				var $demoText = $("a#button.navbar-brand")
 				
 				//Split the text into characters and wrap every character into span element, then convert the whitespaces to whitespace characters.
 				$demoText.html( $demoText.html().replace(/./g, "<span>$&</span>").replace(/\s/g, "&nbsp;"));
@@ -15,7 +15,7 @@ function startAnimation(){
 
 TweenLite.set($demoText, {perspective:400});
 
-tl.staggerFrom(chars, 0.8, {opacity:0.4, scale:1, y:180, rotationX:180, transformOrigin:"0% 50% -50",  ease:Back.easeOut}, .09, "+=0");
+tl.staggerFrom(chars, 0.8, {opacity:0.2, scale:1, y:180, rotationX:180, transformOrigin:"0% 50% -50",  ease:Back.easeOut}, .09, "+=0");
 
 
 
